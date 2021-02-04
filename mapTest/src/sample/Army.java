df7d99786b696e0b33bca361a5b1d027f448d3aa
liamdotcom <liam.vorster@ucdconnect.ie>
2021-02-04 15:29:54 +0000

    Army Class implemented

--- mapTest/src/sample/Army.java
+++ mapTest/src/sample/Army.java
@@ -1,5 +1,43 @@
 package sample;
 
 public class Army {
+    //Instance variables
+    private int armySize;
+    private Player player;
+    private Country country;
 
+    //constructor
+    public Army(Player player, int armySize, Country country){
+        this.player = player;
+        this.armySize = armySize;
+        this.country = country;
+    }
+
+    public int getArmySize() {
+        return armySize;
+    }
+
+    public void setArmySize(int armySize) {
+        this.armySize = armySize;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public void incrementSize(int add){
+        this.armySize+=add;
+    }
 }

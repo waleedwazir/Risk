314d4f4c02dcf905c685146f849284a25934aa0d
liamdotcom <liam.vorster@ucdconnect.ie>
2021-02-11 21:10:34 +0000

    Trello upload
    -Laid out task for Sprint 2

--- NapoleonRisk/src/Main/Main.java
+++ NapoleonRisk/src/Main/Main.java
@@ -1,12 +1,13 @@
 package Main;
 
+import Controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends MainController {
 
     @Override
     public void start(Stage primaryStage) throws Exception{

761b39c54704bbc0fe37812ca913397b078db4b0
waleedwazir <63110077+waleedwazir@users.noreply.github.com>
2021-02-19 16:00:09 +0000

    Updated CardHandle class

--- NapoleonRisk/src/Cards/CardHandle.java
+++ NapoleonRisk/src/Cards/CardHandle.java
@@ -11,6 +11,8 @@ public  class CardHandle {
     private  String type;
 
     public CardHandle(){
+
+        //instantiate deck
         hand = new ArrayList<CardHandle>();
     }
 
@@ -31,15 +33,18 @@ public  class CardHandle {
         return country.getName() + " " + type;
     }
 
+    //returns true if the player can turn in cards
     public boolean can(int c1, int c2, int c3){
         cond = false;
 
         if (hand.size() >= 3) {
             if (hand.get(c1).getType().equals(hand.get(c2).getType()) && hand.get(c1).getType().equals(hand.get(c3).getType())) {
+               //if the three cards are the same type
                 cond = true;
 
             } else if (
                     !hand.get(c1).getType().equals(hand.get(c2).getType()) && !hand.get(c1).getType().equals(hand.get(c3).getType()) && !hand.get(c2).getType().equals(hand.get(c3).getType())) {
+                //if the three cards have different types
                 cond = true;
             }
         }
@@ -51,6 +56,8 @@ public  class CardHandle {
         hand.add(card);
     }
 
+
+    //when you turn in cards this will remove the cards from the hand
     public void removeCards(int c1, int c2, int c3){
         if(can(c1, c2, c3) == true){
             hand.remove(c3);
@@ -61,7 +68,23 @@ public  class CardHandle {
 
         }
     }
+      //returns true if a player must turn in cards
+    public boolean mustTurnIn() {
 
+        cond = false;
+
+        if (hand.size() > 6) {
+            cond = true;
+        }
+        return cond;
+    }
+
+    public String toString(){
+        return getName() + getType() + getCountry();
+    }
+
+
+    //returns the hand
     public ArrayList<CardHandle> getCards() {
         return hand;
     }

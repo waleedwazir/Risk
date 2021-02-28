fda864cd9bb1ef11aef9debe6937f176951701c5
tomkeenan <tom.keenan@ucdconnect.ie>
2021-02-28 19:50:52 +0000

    Added method for getting index of countries in a continent
    -method for figuring out continents

--- NapoleonRisk/src/Map/Countries.java
+++ NapoleonRisk/src/Map/Countries.java
@@ -3,6 +3,7 @@ package Map;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Countries
 {
@@ -31,6 +32,47 @@ public class Countries
     //returns the size of the countries array list.
     public int size(){
         return countries.size();
+    }
+
+    public ArrayList<Integer> continentIndex(String continentName)
+    {
+        ArrayList<Integer> list = new ArrayList();
+
+        if(continentName.equals("Australia"))
+        {
+            Integer[] index = new Integer[] {0,1,2,3};
+            list.addAll(Arrays.asList(index));
+
+        }else if(continentName.equals("Africa"))
+        {
+            Integer[] index = new Integer[] {4,5,6,7,8,9};
+            list.addAll(Arrays.asList(index));
+
+        }else if(continentName.equals("South America"))
+        {
+            Integer[] index = new Integer[] {10,11,12,13};
+            list.addAll(Arrays.asList(index));
+
+        }else if(continentName.equals("Europe"))
+        {
+            Integer[] index = new Integer[] {14,15,16,17,18,19,20};
+            list.addAll(Arrays.asList(index));
+
+        }else if(continentName.equals("North America"))
+        {
+            Integer[] index = new Integer[] {21,22,23,24,25,26,27,28,29};
+            list.addAll(Arrays.asList(index));
+
+        }else if(continentName.equals("Asia"))
+        {
+            Integer[] index = new Integer[] {30,31,32,33,34,35,36,37,38,39,40,41};
+            list.addAll(Arrays.asList(index));
+
+        }else
+        {
+            throw new IllegalArgumentException("Error");
+        }
+        return list;
     }
 
 }
